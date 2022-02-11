1dc2b36a5fef7b63cb7c60ea9deef104ae9fcd80
harsh-nitsgr <91006836+harsh-nitsgr@users.noreply.github.com>
2022-02-11 15:49:31 +0530

    Update palindromic-substrings.cpp

--- string/palindromic-substrings.cpp
+++ string/palindromic-substrings.cpp
@@ -17,3 +17,30 @@ public:
         return count;
     }
 };
+
+//using dynamic programming
+class Solution{
+    public:
+    
+    int countSubstrings(string s){
+        
+        bool dp[s.size()][s.size()];
+        int count=0;
+        
+        for(int g=0;g<s.size();g++){
+            for(int i=0,j=g;j<s.size();j++,i++){
+                if(g==0) dp[i][j]=1;
+                else if(g==1){
+                    if(s[i]==s[j])dp[i][j]=1;
+                    else dp[i][j]=0;
+                }
+                else{
+                    if(s[i]==s[j] && dp[i+1][j-1]) dp[i][j]=1;
+                    else dp[i][j]=0;
+                }
+                if(dp[i][j]==1)count++;
+            }
+        }
+        return count;
+    }  
+};
