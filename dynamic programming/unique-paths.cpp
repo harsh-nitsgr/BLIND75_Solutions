5800b6fee68f840d75af2b87692c2094c3df0d1d
harsh-nitsgr <91006836+harsh-nitsgr@users.noreply.github.com>
2022-02-19 09:02:07 -0800

    Add files via upload

---
+++ dynamic programming/unique-paths.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+        int dp[m][n];
+        
+        for(int i=0,j=n-1; i<m; i++) dp[i][j]=1;
+        for(int i=m-1,j=0; j<n; j++) dp[i][j]=1;
+        
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
+    }
+};
