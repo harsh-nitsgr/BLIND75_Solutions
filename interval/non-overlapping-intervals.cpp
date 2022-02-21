d488ad8068bf7911569d9a7ee682ed7d5ba6e4f3
harsh-nitsgr <91006836+harsh-nitsgr@users.noreply.github.com>
2022-02-21 01:18:39 -0800

    Add files via upload

---
+++ interval/non-overlapping-intervals.cpp
@@ -0,0 +1,19 @@
+bool comp(vector<int> &a,vector<int> &b) {
+	return a[1]<b[1];
+}
+class Solution {
+public:
+	int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+		int ans=-1;      
+		if(intervals.size()==0) return 0;       
+		sort(intervals.begin(),intervals.end(),comp);    
+		
+        int end0=intervals[0][1];
+
+		for(auto &i: intervals) {
+			if(end0 >i[0]) ans++;
+            else end0=i[1];
+		}
+		return ans; 
+	}
+};
